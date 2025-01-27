2e2182689b88645e5150500c5e64c4ceb1bdb986
Lucas-Viesan <vieiratm01@hotmail.com>
2025-01-27 11:12:47 -0300

    Implementado CRUD-Produto

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,86 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo é obrigatório!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo é obrigatório!")
+	private String informacoesProduto;
+	
+	@Column(nullable = false)
+	private Double preco;
+	
+	@Column(nullable = false)
+	private Integer quantidadeEstoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getInformacoesProduto() {
+		return informacoesProduto;
+	}
+
+	public void setInformacoesProduto(String informacoesProduto) {
+		this.informacoesProduto = informacoesProduto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
